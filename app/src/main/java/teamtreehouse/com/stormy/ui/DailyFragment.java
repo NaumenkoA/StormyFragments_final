6ec320496d8a2d2be921311049e35da9cd14de42
NaumenkoA <goodwin-na@mail.ru>
2016-11-20 14:24:01 +0300

    Initial commit

---
+++ app/src/main/java/teamtreehouse/com/stormy/ui/DailyFragment.java
@@ -0,0 +1,34 @@
+package teamtreehouse.com.stormy.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.Arrays;
+
+import teamtreehouse.com.stormy.R;
+import teamtreehouse.com.stormy.adapters.DayAdapter;
+import teamtreehouse.com.stormy.weather.Day;
+
+public class DailyFragment extends Fragment {
+private Day [] mDays;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_daily_forecast, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.dailyListView);
+        Bundle bundle = getArguments();
+        Parcelable[] parcelables = bundle.getParcelableArray(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+        DayAdapter dayAdapter = new DayAdapter (getActivity(), mDays);
+        listView.setAdapter(dayAdapter);
+        listView.setEmptyView(view.findViewById(R.id.emptyTextView));
+        return view;
+    }
+}
